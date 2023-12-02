c59879b8f50a6dfb9d4db547c8609f72bde2e4f3
jindraivanek <jindra.ivanek@gmail.com>
2023-12-02 22:02:54 +0100

    runAll script

---
+++ 2023/fsharp/runAll.fsx
@@ -0,0 +1,15 @@
+#r "nuget: CliWrap"
+
+let scripts = 
+    System.IO.DirectoryInfo(__SOURCE_DIRECTORY__).GetDirectories() 
+    |> Seq.collect (fun d -> System.IO.DirectoryInfo(d.FullName).GetFiles("solve.fsx") |> Seq.map (fun x -> d, x.FullName))
+
+scripts |> Seq.iter (fun (d, x) ->
+    let sb = System.Text.StringBuilder() 
+    printfn $"DAY {d.Name}"
+    CliWrap.Cli.Wrap("dotnet").WithArguments($"fsi \"{x}\"")
+        .WithWorkingDirectory(d.FullName)
+        .WithStandardOutputPipe(CliWrap.PipeTarget.ToStringBuilder(sb))
+        .ExecuteAsync().Task.Result |> ignore
+    printfn "%s" <| sb.ToString()
+)
