43049708bfdf03ef0c76b4ca82319325c0729b05
jindraivanek <jindra.ivanek@gmail.com>
2023-12-12 22:33:31 +0100

    Day 12

--- 2023/fsharp/12/solve.fsx
+++ 2023/fsharp/12/solve.fsx
@@ -1,8 +1,22 @@
 #time
 
+let lines = System.IO.File.ReadAllLines("input")
+//let lines = System.IO.File.ReadAllLines("sample")
+
 let teePrint x =
     printfn $"%A{x}"
     x
+
+let memoizeRec f =
+    let cache = System.Collections.Generic.Dictionary<_, _>()
+    let rec f' x =
+        match cache.TryGetValue x with
+        | true, v -> v
+        | _ ->
+            let v = f f' x
+            cache.Add(x, v)
+            v
+    f'
 
 let chunkBy f xs =
     let rec go acc xs =
@@ -15,48 +29,18 @@ let chunkBy f xs =
             | ys::acc -> go ((x::ys)::acc) xs
     go [] xs
 
-let pairComb xs ys = 
-    let rec pairComb' acc xs ys =
-        //printfn $"%A{acc} %A{xs} %A{ys}"
-        let hashPrefix xs = xs |> List.takeWhile ((=) '#') |> List.length
-        match xs, ys with
-        | gs, [] when gs |> List.exists (fun (_, g) -> g |> List.contains '#') -> []
-        | _, [] -> acc
-        | [], _ -> []
-        | (_,[])::xs, ys -> pairComb' acc xs ys
-        | (i,g)::xs, y::ys ->
-            let lg = List.length g
-            let hashPrefix = hashPrefix g
-            let validBlock = (if y > lg then None else List.skip y g |> List.tryHead) = Some '?'
-            match (i,g)::xs, y::ys with
-            | (i, (('#'::_) as g))::xs, y::ys when lg > y + 1 && validBlock && y >= hashPrefix -> 
-                let acc' = acc |> List.map (fun zs -> ((i,y)::zs))
-                pairComb' acc' ((i+y+1, List.skip (y+1) g) :: xs) (ys)
-            | (i, (('#'::_) as g))::xs, y::ys when lg = y || (lg = y + 1 && validBlock) ->
-                let acc' = acc |> List.map (fun zs -> ((i,y)::zs))
-                pairComb' acc' xs ys
-            | (i, (('#'::_) as g))::_, _ -> []
-            | (i, g)::xs, y::ys when lg > y + 1 && validBlock -> 
-                let acc' = acc |> List.map (fun zs -> ((i,y)::zs))
-                pairComb' acc ((i+1, List.skip 1 g) :: xs) (y::ys) @ pairComb' acc' ((i+y+1, List.skip (y+1) g) :: xs) (ys)
-            | (i, g)::xs, y::ys when lg = y || (lg = y + 1 && validBlock) ->
-                let acc' = acc |> List.map (fun zs -> ((i,y)::zs))
-                pairComb' acc ((i+1, List.skip 1 g) :: xs) (y::ys) @ pairComb' acc' xs ys
-            | (i,g)::xs, ys ->
-                pairComb' acc ((i+1, List.skip 1 g) :: xs) ys
-    pairComb' [[]] xs ys |> List.filter ((<>) [])
-
 let pairCombNum xs ys = 
     let n = xs |> List.last |> (fun (i, x) -> i + List.length x + 1)
     let ySum ys = List.sum ys + List.length ys - 1
-    let rec pairComb' acc xs ys =
+    let rec pairComb' = memoizeRec <| fun recf (xs, ys) ->
+        let recf x y = recf (x, y)
         //printfn $"%A{acc} %A{xs} %A{ys}"
         let hashPrefix xs = xs |> List.takeWhile ((=) '#') |> List.length
         match xs, ys with
-        | gs, [] when gs |> List.exists (fun (_, g) -> g |> List.contains '#') -> 0
-        | _, [] -> acc
-        | [], _ -> 0
-        | (_,[])::xs, ys -> pairComb' acc xs ys
+        | gs, [] when gs |> List.exists (fun (_, g) -> g |> List.contains '#') -> 0L
+        | _, [] -> 1L
+        | [], _ -> 0L
+        | (_,[])::xs, ys -> recf xs ys
         | (i,_)::_, ys when n-i+1 < ySum ys -> 0
         | (i,g)::xs, y::ys ->
             let lg = List.length g
@@ -64,21 +48,17 @@ let pairCombNum xs ys =
             let validBlock = (if y > lg then None else List.skip y g |> List.tryHead) = Some '?'
             match (i,g)::xs, y::ys with
             | (i, (('#'::_) as g))::xs, y::ys when lg > y + 1 && validBlock && y >= hashPrefix -> 
-                pairComb' acc ((i+y+1, List.skip (y+1) g) :: xs) (ys)
+                recf ((i+y+1, List.skip (y+1) g) :: xs) (ys)
             | (i, (('#'::_) as g))::xs, y::ys when lg = y || (lg = y + 1 && validBlock) ->
-                pairComb' acc xs ys
+                recf xs ys
             | (i, (('#'::_) as g))::_, _ -> 0
             | (i, g)::xs, y::ys when lg > y + 1 && validBlock -> 
-                pairComb' acc ((i+1, List.skip 1 g) :: xs) (y::ys) + pairComb' acc ((i+y+1, List.skip (y+1) g) :: xs) (ys)
+                recf ((i+1, List.skip 1 g) :: xs) (y::ys) + recf ((i+y+1, List.skip (y+1) g) :: xs) (ys)
             | (i, g)::xs, y::ys when lg = y || (lg = y + 1 && validBlock) ->
-                pairComb' acc ((i+1, List.skip 1 g) :: xs) (y::ys) + pairComb' acc xs ys
+                recf ((i+1, List.skip 1 g) :: xs) (y::ys) + recf xs ys
             | (i,g)::xs, ys ->
-                pairComb' acc ((i+1, List.skip 1 g) :: xs) ys
-    pairComb' 1 xs ys
-
-
-let lines = System.IO.File.ReadAllLines("input")
-//let lines = System.IO.File.ReadAllLines("sample")
+                recf ((i+1, List.skip 1 g) :: xs) ys
+    pairComb' (xs, ys)
 
 let parsed =
     lines
@@ -92,35 +72,18 @@ let groups xs =
     xs |> Seq.indexed |> Seq.toList |> chunkBy (fun (_, c) -> c='.') |> List.filter (fun xs -> (List.head >> snd) xs <> '.')
     |> List.map (fun xs -> xs |> List.map fst |> List.min, xs |> List.map snd)
 
-let lineComb xs cs =
-    let xs = xs |> Seq.toList
-    let g = groups xs //|> List.map teePrint
-    let r = pairComb g cs //|> teePrint
-    // r |> Seq.iter (fun rs ->
-    //     let idx = rs |> Seq.collect (fun (i,n) -> [i .. i+n-1]) |> set
-    //     let s = xs |> Seq.mapi (fun i c -> if idx.Contains i then '#' else '.') |> Seq.toList //|> teePrint
-    //     List.zip xs s |> List.iter (function | ('.', '#') | ('#', '.') -> printfn $"%A{xs}"; failwith "mismatch" | _ -> ())
-    // )
-    r |> Seq.length |> teePrint
-
 let lineCombNum xs cs =
     let xs = xs |> Seq.toList
     let g = groups xs //|> List.map teePrint
     let r = pairCombNum g cs //|> teePrint
-    // r |> Seq.iter (fun rs ->
-    //     let idx = rs |> Seq.collect (fun (i,n) -> [i .. i+n-1]) |> set
-    //     let s = xs |> Seq.mapi (fun i c -> if idx.Contains i then '#' else '.') |> Seq.toList //|> teePrint
-    //     List.zip xs s |> List.iter (function | ('.', '#') | ('#', '.') -> printfn $"%A{xs}"; failwith "mismatch" | _ -> ())
-    // )
-    r |> teePrint
+    r //|> teePrint
 
 
 let unfold (xs: string, cs) =
-    let xs = xs |> Seq.map (fun c -> String.replicate 5 (string c)) |> String.concat "?"
-    let cs = cs |> List.collect (fun x -> List.replicate 5 x)
+    let xs = Seq.replicate 5 xs |> String.concat "?"
+    let cs = cs |> List.replicate 5 |> List.concat
     xs, cs
-let part1() = parsed |> Seq.map teePrint |> Seq.map (fun (xs, cs) -> lineCombNum xs cs) |> Seq.sum
-printfn $"{part1()}" //7191
-let part2() = parsed |> Seq.map unfold |> Seq.map teePrint |> Seq.map (fun (xs, cs) -> lineCombNum xs cs |> int64) |> Seq.sum
-
+let part1() = parsed |> Seq.map (fun (xs, cs) -> lineCombNum xs cs) |> Seq.sum
+printfn $"{part1()}"
+let part2() = parsed |> Seq.map unfold |> Seq.map (fun (xs, cs) -> lineCombNum xs cs |> int64) |> Seq.sum
 printfn $"{part2()}"
