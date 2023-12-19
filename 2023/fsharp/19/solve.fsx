60330fc3cada6a360bb1d89ef2b6883fdf8c49fd
jiv <jiv@ciklum.com>
2023-12-19 15:12:13 +0100

    Day 19

--- 2023/fsharp/19/solve.fsx
+++ 2023/fsharp/19/solve.fsx
@@ -1,7 +1,7 @@
 #time
 
-//let lines = System.IO.File.ReadAllLines("input")
-let lines = System.IO.File.ReadAllLines("sample")
+let lines = System.IO.File.ReadAllLines("input")
+//let lines = System.IO.File.ReadAllLines("sample")
 
 open System.Text.RegularExpressions
 
@@ -69,35 +69,68 @@ let limitRanges r = function
         | '<' -> r |> Map.add s (lb, min ub x)
         | '>' -> r |> Map.add s (max lb x, ub)
 
-let rec acceptingRanges wName r =
-    let w = Map.find wName workflows
-    w |> List.collect (fun (t, o) ->
+let reverseOp = function
+    | None -> None
+    | Some (s, x, op) -> 
+        match op with
+        | '<' -> s, x-1, '>'
+        | '>' -> s, x+1, '<'
+        |> Some
+
+let rec acceptingRanges wNames r =
+    let w = Map.find (List.head wNames) workflows
+    w |> List.fold (fun (s, acc) (t, o) ->
+        let cont = limitRanges s (reverseOp o)
         match t with
         | "A" -> 
-            let r = limitRanges r o
-            printfn "%A" r
-            [r]
-        | "R" -> []
-        | t -> acceptingRanges t (limitRanges r o)
-    )
+            let r = limitRanges s o
+            //printfn "%A %A" wNames r
+            cont, r :: acc
+        | "R" -> cont, acc
+        | t -> cont, acc @ acceptingRanges (t::wNames) (limitRanges s o)
+    ) (r, [])
+    |> snd
 
-let combineRanges r1 r2 =
-    if r1 <> r2 && Map.forall (fun k (lb1, ub1) -> Map.find k r2 |> fun (lb2, ub2) -> (lb1 <= lb2 && lb2 <= ub1) || (lb2 <= lb1 && lb1 <= ub2)) r1 then
-        r1 |> Map.map (fun k (lb1, ub2) -> let lb, ub = Map.find k r2 in (min lb1 lb), (max ub2 ub))|> Some
+let intersectRanges (lb1, ub1) (lb2, ub2) =
+    let lb = max lb1 lb2
+    let ub = min ub1 ub2
+    if lb <= ub then Some (lb, ub) else None
+
+let rangeMapSize m = m |> Map.values |> Seq.map (fun (lb, ub) -> ub - lb - 1 |> int64) |> Seq.reduce (*)
+let intersectRangeMaps m1 m2 =
+    if m1 <> m2 && Map.forall (fun k r1 -> Map.find k m2 |> fun r2 -> (intersectRanges r1 r2).IsSome) m1 then
+        let r = m1 |> Map.map (fun k r1 -> let r2 = Map.find k m2 in intersectRanges r1 r2 |> Option.get)
+        Some r
     else None
 
-let rec combineRangesList xs =
-    match xs |> List.tryPick (fun r1 -> xs |> List.tryPick (fun r2 -> combineRanges r1 r2)) with
-    | Some 
+let pairWith xs ys f =
+    xs |> Seq.collect (fun x -> ys |> Seq.filter (snd >> (<>)x) |> Seq.choose (fun (s, y) -> 
+        if Set.contains x s then None else f x y |> Option.map (fun z -> Set.add x s, z)))
+
+let rec rangeMapsSize size acc xs =
+    //printfn "SIZE %A" size
+    let label x = xs |> List.findIndex ((=)x)
+    if size = 1 then
+        let singlesSize = xs |> Seq.sumBy rangeMapSize
+        //printfn "%A" singlesSize
+        singlesSize + rangeMapsSize 2 (xs |> List.map (fun x -> set [x], x)) xs
+    else
+        let pairsIntersects = pairWith xs acc intersectRangeMaps |> Seq.distinctBy fst |> Seq.toList
+        let sign = if size % 2 = 1 then 1L else -1L
+        if pairsIntersects.IsEmpty then 0L else
+        let x = (pairsIntersects |> Seq.sumBy (snd>>rangeMapSize)) * sign 
+        //printfn "%A %A" x pairsIntersects.Length
+        //pairsIntersects |> Seq.iter (fun (s, m) -> printfn "[%A] %A %A" (s |> Set.map label) m (rangeMapSize m))
+        x + rangeMapsSize (size + 1) pairsIntersects xs
 
 let startRanges = "xmas" |> Seq.map (fun c -> string c, (0, 4001)) |> Map.ofSeq
-let rangesCombination = startRanges |> acceptingRanges "in" |> combineRangesList
-printfn "%A" rangesCombination
+let rangesCombination = startRanges |> acceptingRanges ["in"] |> List.distinct
+//printfn "%A" (List.indexed rangesCombination)
 
 let part1 = parts |> List.filter (fun part -> partAccepted part "in") |> List.sumBy (Map.values >> Seq.sum)
 let part2 = 
     rangesCombination
-    |> Seq.sumBy (Map.values >> Seq.map (fun (lb, ub) -> ub - lb - 1 |> int64) >> Seq.reduce (*))
+    |> rangeMapsSize 1 []
 
 printfn $"{part1}" // 287054
-printfn $"{part2}"
+printfn $"{part2}" // 131619440296497
