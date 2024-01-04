32d0e4689f386edf53da7805258cf8f5aba11f09
jiv <jiv@ciklum.com>
2024-01-04 17:44:27 +0100

    wip

--- 2023/fsharp/02/solve.fsx
+++ 2023/fsharp/02/solve.fsx
@@ -14,8 +14,13 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 
 ...
 
-The Elf would first like to know which games would have been possible 
-if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
+--- Part One ---
+Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+
+--- Part Two ---
+The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
+
+For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
 *)
 
 #time
