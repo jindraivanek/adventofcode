6501098edbfee3e2efbe2022fa999ee2bf39c395
jiv <jiv@ciklum.com>
2024-01-04 17:25:34 +0100

    day2 slides

--- 2023/fsharp/02/solve.fsx
+++ 2023/fsharp/02/solve.fsx
@@ -1,3 +1,23 @@
+(*
+--- Day 2: Cube Conundrum ---
+You're launched high into the atmosphere! The apex of your trajectory just barely reaches the surface of a large island floating in the sky. You gently land in a fluffy pile of leaves. It's quite cold, but you don't see much snow. An Elf runs over to greet you.
+
+The Elf explains that you've arrived at Snow Island and apologizes for the lack of snow. He'll be happy to explain the situation, but it's a bit of a walk, so you have some time. They don't get many visitors up here; would you like to play a game in the meantime?
+...
+
+SAMPLE:
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+
+...
+
+The Elf would first like to know which games would have been possible 
+if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
+*)
+
 #time
 open System.Text.RegularExpressions
 
@@ -10,17 +30,6 @@ let (|Match|_|) (pat: string) (inp: string) =
         None
 
 let lines = System.IO.File.ReadAllLines("input")
-
-(*
-SAMPLE:
-Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-...
-The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
-*)
 
 type Cube =
     | Red
