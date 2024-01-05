b09f6ce10dfbd9f947aef938272e22a5ba18e22b
jiv <jiv@ciklum.com>
2024-01-05 15:45:44 +0100

    lzgit - 2024-01-05T14:45:44.160Z

--- 2023/fsharp/13/solve.fsx
+++ 2023/fsharp/13/solve.fsx
@@ -1,3 +1,36 @@
+(*
+--- Day 13: Point of Incidence ---
+
+...
+
+To find the reflection in each pattern, you need to find a perfect reflection across either a horizontal line between two rows or across a vertical line between two columns.
+
+In the first pattern, the reflection is across a vertical line between two columns; arrows on each of the two columns point at the line between the columns:
+
+123456789
+    ><   
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+    ><   
+123456789
+
+To summarize your pattern notes, add up the number of columns to the left of each vertical line of reflection; to that, also add 100 multiplied by the number of rows above each horizontal line of reflection.
+
+Find the line of reflection in each of the patterns in your notes. What number do you get after summarizing all of your notes?
+
+--- Part Two ---
+Upon closer inspection, you discover that every mirror has exactly one smudge: exactly one . or # should be the opposite type.
+
+> (smudge = exactly one error in reflection)
+
+In each pattern, fix the smudge and find the different line of reflection. What number do you get after summarizing the new reflection line in each pattern in your notes?
+*)
+
 #time
 
 let lines = System.IO.File.ReadAllLines("input")
@@ -15,11 +48,11 @@ let chunkBy f xs =
 
     go [] xs
 
-let diffCount xs ys =
-    List.zip xs ys |> List.filter (fun (x, y) -> x <> y) |> List.length
-
 let patterns =
     lines |> Seq.toList |> chunkBy ((<>) "") |> List.map (List.map Seq.toList)
+
+let diffCount xs ys =
+    List.zip xs ys |> List.filter (fun (x, y) -> x <> y) |> List.length
 
 let findReflectionWithDiffCount k pattern =
     let sumDiffs xs ys =
