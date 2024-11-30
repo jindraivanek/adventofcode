311d0f962503db82241ceb6775380519d229f8c0
jindraivanek <jindra.ivanek@gmail.com>
2024-11-30 11:05:10 +0100

    2024 init

--- 2024/Common.fs
+++ 2024/Common.fs
@@ -14,11 +14,17 @@ type Day<'s1, 's2> = {
 
 let readLines (day: int) = System.IO.File.ReadAllLines($"%s{__SOURCE_DIRECTORY__}/../input/2024/%02i{day}.txt")
 
+let benchmark label f =
+    let sw = System.Diagnostics.Stopwatch.StartNew()
+    let x = f()
+    sw.Stop()
+    printfn $"%s{label}: %A{x} [%A{sw.Elapsed}]"
+
 let runSolution (sol: Solution<'s>) (lines: string[]) =
     let init = sol.Init lines 
     init |> Seq.unfold (fun s -> sol.Step s |> Option.map (fun x -> x, x)) |> Seq.tryLast |> Option.defaultValue init |> sol.Result
 
 let runDay (day: Day<'s1, 's2>) =
     let lines = readLines day.Day
-    printfn "Day %i - Part 1: %s" day.Day (runSolution day.Part1 lines)
-    printfn "Day %i - Part 2: %s" day.Day (runSolution day.Part2 lines)
+    benchmark $"Day %i{day.Day} - Part 1" (fun () -> runSolution day.Part1 lines)
+    benchmark $"Day %i{day.Day} - Part 2" (fun () -> runSolution day.Part2 lines)
--- 2024/Program.fs
+++ 2024/Program.fs
@@ -2,11 +2,4 @@ open Common
 let runAll() =
     runDay day01.sol
  
-
-let benchmark f =
-    let sw = System.Diagnostics.Stopwatch.StartNew()
-    f()
-    sw.Stop()
-    printfn $"%A{sw.Elapsed}"
-
-benchmark runAll
+Common.benchmark "TOTAL" runAll
