d2e2fccf7a71ded828d9aaa9cc780fbc9430902c
jindraivanek <jindra.ivanek@gmail.com>
2024-12-02 07:21:58 +0100

    day02

---
+++ 2024/02.fs
@@ -0,0 +1,33 @@
+module day02
+open Common
+
+let parseInput (lines: string[]) =
+    lines |> Seq.map (fun s -> s.Split(' ', System.StringSplitOptions.RemoveEmptyEntries) |> Array.map int |> Array.toList) |> Seq.toList
+
+let diffs xs = xs |> List.pairwise |> List.map (fun (a, b) -> b - a)
+
+let isSafe xs =
+    let d = diffs xs
+    List.forall (fun x -> x >= 1 && x <= 3) d || List.forall (fun x -> x <= -1 && x >= -3) d
+
+let part1 lines = 
+   parseInput lines |> List.filter (fun xs -> isSafe xs )
+
+let part2 lines = 
+    parseInput lines |> List.filter (fun xs -> 
+        let variants = [0..List.length xs] |> List.map (fun i -> xs |> List.indexed |> List.filter (fun (j, _) -> i <> j) |> List.map snd)
+        variants |> List.exists (fun xs -> isSafe xs))
+
+let sol = {
+    Day = 2
+    Part1 = {
+        Init = part1
+        Step = fun _ -> None
+        Result = Seq.length >> string
+    }
+    Part2 = {
+        Init = part2
+        Step = fun _ -> None
+        Result = Seq.length >> string
+    }
+}
--- 2024/Program.fs
+++ 2024/Program.fs
@@ -1,5 +1,6 @@
 open Common
 let runAll() =
     runDay day01.sol
+    runDay day02.sol
  
 Common.benchmark "TOTAL" runAll
