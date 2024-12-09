55cc4e41d67f6f25c2cb0c6848249e6a1154246f
jindraivanek <jindraivanek>
2024-12-09 14:17:27 +0100

    Day09 part2

--- 2024/09.fs
+++ 2024/09.fs
@@ -2,38 +2,60 @@ module day09
 open Common
 
 let parseInput (lines: string[]) =
-    lines |> Array.map (fun s -> s |> Seq.map (string >> int) |> Seq.indexed |> Seq.collect (fun (i, x) -> Seq.replicate x (if i%2 = 1 then None else (Some (i/2)))) |> Seq.toList)
-    |> Seq.head
+    let allBlocks = lines |> Seq.head |> Seq.map (string >> int) |> Seq.indexed
+    let blocks = allBlocks |> Seq.filter (fun (i, _) -> i%2 = 0) |> Seq.map (fun (i, x) -> i/2, x) |> Map.ofSeq
+    blocks, allBlocks |> Seq.collect (fun (i, x) -> Seq.replicate x (if i%2 = 1 then None else (Some (i/2)))) |> Seq.toList
 
-let moveBLocks xs =
-    let swap i j (arr: _[]) =
-        //printfn "swap %i %i %A %A" i j arr[i] arr[j]
-        let tmp = arr[i]
-        arr[i] <- arr[j]
-        arr[j] <- tmp
-    let rs = xs |> List.indexed |> List.rev
+let moveBlocks wholeBlock (blocks: Map<int,int>) xs =
+    let swap i j k (arr: _[]) =
+        for l in 0..k-1 do
+        let tmp = arr[i+l]
+        arr[i+l] <- arr[j+l]
+        arr[j+l] <- tmp
+    let freeSpaces (arr: _[]) =
+        seq {
+        let mutable i = 0
+        while i < arr.Length do
+            let x = Seq.initInfinite (fun j -> arr[i+j]) |> Seq.takeWhile ((=) None) |> Seq.length
+            if x > 0 then i, x
+            i <- i + (max x 1)
+        }
+        |> Seq.toArray
+    
+    let rs =
+        if wholeBlock then
+            blocks |> Map.toSeq |> Seq.sortDescending |> Seq.map (fun (x, l) -> List.findIndex ((=) (Some x)) xs, l, x) |> Seq.toList
+        else xs |> List.indexed |> List.rev |> List.choose (fun (i, x) -> x |> Option.map (fun x -> i, x)) |> List.map (fun (i, x) -> i, 1, x)
     let arr = xs |> List.toArray
-    let rec loop i rs =
-        //printfn "%A" arr
-        if i >= arr.Length then arr |> List.ofArray else
-        match arr[i], rs with
-        | _, [] -> arr |> List.ofArray
-        | _, (j, _):: _ when i >= j -> arr |> List.ofArray
-        | _, (_, None)::rs -> loop i rs
-        | Some _, rs -> loop (i+1) rs
-        | None, (j, Some r)::rs -> swap i j arr; loop (i+1) rs
-    loop 0 rs
+    let mutable freeSpacesMut = freeSpaces arr
+    let findLeftMostFreeSpace k =
+        freeSpacesMut
+        |> Seq.tryFind (fun (_, x) -> x >= k)
+    let rec loop rs =
+        rs |> List.iter (fun (j, k, _) ->
+            match findLeftMostFreeSpace k with
+            | Some (i, x) when i < j ->
+                swap i j k arr
+                let index = freeSpacesMut |> Seq.findIndex (fun (l, _) -> l = i)
+                freeSpacesMut[index] <- (i+k, x-k)
+            | _ -> ()
+        )
+        arr |> List.ofArray
+        
+    loop rs
         
 let part1 lines = 
-   let xs = parseInput lines |> moveBLocks
+   let xs = parseInput lines ||> moveBlocks false
    xs |> Seq.mapi (fun i x -> x |> Option.defaultValue 0 |> (*) i |> int64)
    |> Seq.sum
 
 let part2 lines = 
-    parseInput lines
+   let xs = parseInput lines ||> moveBlocks true
+   xs |> Seq.mapi (fun i x -> x |> Option.defaultValue 0 |> (*) i |> int64)
+   |> Seq.sum
 
 let sol = {
     Day = 9
-    Part1 = solution part1 (string)
-    Part2 = solution part2 (Seq.length >> string)
+    Part1 = solution part1 string
+    Part2 = solution part2 string
 }
