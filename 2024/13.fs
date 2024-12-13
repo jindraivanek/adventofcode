c5afbd3d35b3a13296e70de2215cab4f65f52e7b
jindraivanek <jindra.ivanek@gmail.com>
2024-12-13 12:50:42 +0100

    day13 part2

--- 2024/13.fs
+++ 2024/13.fs
@@ -7,27 +7,31 @@ let parseInput (lines: string[]) =
         | Match "Button A: X\+([0-9]+), Y\+([0-9]+)" [ ax; ay ],
           Match "Button B: X\+([0-9]+), Y\+([0-9]+)" [ bx; by ],
           Match "Prize: X=([0-9]+), Y=([0-9]+)" [ x; y ]
-          -> (int ax, int ay), (int bx, int by), (int x, int y)
+          -> (int64 ax, int64 ay), (int64 bx, int64 by), (int64 x, int64 y)
     )
 
 let solve (ax, ay) (bx, by) (x, y) =
-    let n = max (max (x / ax) (y / ay)) (max (x / bx) (y / by))
-    let n = n + 1
-    Seq.allPairs [0..n] [0..n] |> Seq.filter (fun (a,b) ->
-        let tx = a * ax + b * bx
-        let ty = a * ay + b * by
-        //printfn "%A %A %A %A = x=%A y=%A" a b tx ty x y
-        x = tx && y = ty
-    ) |> Seq.map (fun (a,b) -> printfn "%A %A" a b; 3*a + b) |> Seq.toList |> function | [] -> None | x -> Some (List.min x) 
+    let A = ax
+    let B = bx
+    let C = ay
+    let D = by
+    let b = (-A*y + C*x) / (C*B - A*D)
+    let a = (x - b*B) / A
+    let tx = a * ax + b * bx
+    let ty = a * ay + b * by
+    //printfn "%A" (a, b, tx, ty, x, y)
+    if x = tx && y = ty then Some (3L*a + b) else None
+    
 let part1 lines = 
    let xs = parseInput lines
    xs |> List.choose (fun (a, b, t) -> solve a b t) 
 
 let part2 lines = 
-    parseInput lines
+   let xs = parseInput lines |> List.map (fun (a, b, (x, y)) -> a, b, (x+10000000000000L, y+10000000000000L))
+   xs |> List.choose (fun (a, b, t) -> solve a b t) 
 
 let sol = {
     Day = 13
     Part1 = solution part1 (Seq.sum >> string)
-    Part2 = solution part2 (Seq.length >> string)
+    Part2 = solution part2 (Seq.sum >> string)
 }
