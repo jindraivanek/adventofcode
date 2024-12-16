7b6d89c852d458f4756f78083f7014bed6b83224
jindraivanek <jindraivanek>
2024-12-16 13:57:52 +0100

    day 16

--- 2024/16.fs
+++ 2024/16.fs
@@ -25,8 +25,9 @@ let parseInput (lines: string[]) =
 
     let neighF (p, di) =
         let d = dir di
-        [(posPlus p d, di), 1; (p, di + 1), 1000; (p, di - 1), 1000]
-        |> List.filter (fun ((v, _), _) -> Set.contains v floors)
+        let moves = [((posPlus p d), di), 1] |> List.filter (fun ((v, _), _) -> Set.contains v floors)
+        let rotates = [(p, (di + 1) % dirs.Length), 1000; (p, (di - 1) % dirs.Length), 1000] |> List.filter (fun ((p, i), _) -> Set.contains (posPlus p (dir i)) floors)
+        moves @ rotates
 
     let isFinish (p, _) = Set.contains p finish
 
@@ -37,7 +38,9 @@ let parseInput (lines: string[]) =
 let dijkstra (initNodes: ('n * int) list) (neighF: 'n -> ('n * int) list) (finishCond: 'n -> bool) resultMap =
     let pq = System.Collections.Generic.PriorityQueue<'n, int>()
     let visited = System.Collections.Generic.HashSet<_>()
-    let score = System.Collections.Generic.Dictionary<_, _ Set>()
+    let paths = System.Collections.Generic.Dictionary<_, _ * _ Set>()
+    let finishPaths = System.Collections.Generic.HashSet<_>()
+    let mutable opt = None
 
     let dequeue () =
         let (success, n, p) = pq.TryDequeue()
@@ -51,52 +54,60 @@ let dijkstra (initNodes: ('n * int) list) (neighF: 'n -> ('n * int) list) (finis
     initNodes
     |> Seq.iter (fun (n, p) ->
         pq.Enqueue(n, p)
-        score.Add(n, set [ [ n ] ]))
+        let (v, _) = n
+        paths.Add(n, (p, set [ v ])))
 
+    let result p = p, finishPaths |> Seq.collect id |> set |> Set.count 
+    
     let rec step () =
-        match dequeue () with
-        | Some((node), p) when visited.Contains(node) -> step ()
-        | None ->
-            score
-            |> Seq.filter (fun kvp -> finishCond kvp.Key)
-            |> Seq.sumBy (fun kvp -> kvp.Value |> Set.map resultMap |> Set.count)
-        | Some((node), p) when finishCond node -> p
-        | Some((node), p) ->
+        match dequeue (), opt with
+        | Some(node, p), _ when visited.Contains(node) ->
+            //printfn $"Already visited: %A{node}, %A{p}"
+            step ()
+        | None, Some o -> result o |> resultMap
+        | Some(node, p), Some o when finishCond node && p > o -> result p |> resultMap
+        | Some(node, p), None when finishCond node ->
+            //printfn $"FINISH %A{node} %A{p}"
+            opt <- Some p
+            finishPaths.Add(snd paths[node]) |> ignore
+            step()
+        | Some((node), p), _ ->
+            //printfn $"%A{node}, %A{p}"
             visited.Add(node) |> ignore
 
             neighF node
-            |> Seq.iter (fun (n, p') ->
-                let newScore = score[node] |> Set.map (fun p -> n :: p)
-
-                if score.ContainsKey n then
-                    score[n] <- Set.union score[n] newScore
+            |> Seq.iter (fun ((v,_) as n, p') ->
+                let newPath = Set.add v (snd paths[node])
+                let c = p + p'
+                
+                if paths.ContainsKey n then
+                    if c = fst paths[n] then
+                        paths[n] <- c, snd paths[n] + newPath
                 else
-                    score.Add(n, newScore)
+                    paths.Add(n, (c, newPath))
 
-                pq.Enqueue(n, p + p')
-
-            //if finishCond n then
-            //printfn $"%A{(n, score[n])}"
+                pq.Enqueue(n, c)
             )
 
             step ()
+        | None, None -> failwith "No solution"
 
     step ()
 
 
 let part1 lines =
     let s = parseInput lines
-    let r = dijkstra s.Starts s.NeighF s.IsFinish List.last
+    let r = dijkstra s.Starts s.NeighF s.IsFinish fst
     r
 
 let part2 lines =
     let s = parseInput lines
-    let r = dijkstra s.Starts s.NeighF s.IsFinish id
+    let r = dijkstra s.Starts s.NeighF s.IsFinish snd
     r
 
 
 let sol = {
     Day = 16
     Part1 = solution part1 string
-    Part2 = solution part2 (string)
+    Part2 = solution part2 (string) //549 too high, 429 too low
 }
