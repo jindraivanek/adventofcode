ad8dca43986c990bc61699ae7d04e85ee43532a1
jindraivanek <jindra.ivanek@gmail.com>
2024-12-23 01:19:45 +0100

    day 22

--- 2024/22.fs
+++ 2024/22.fs
@@ -17,19 +17,31 @@ let genSecret x =
     |> genSecretPart ((*) 2048L)
 
 let rec secretSeq x = seq {
-    let y = genSecret x
-    y
-    yield! secretSeq y 
+    yield x
+    yield! secretSeq (genSecret x) 
 }
 
 let part1 lines = 
-   parseInput lines |> Seq.map (secretSeq >> Seq.nth 1999)
+   parseInput lines |> Seq.map (secretSeq >> Seq.nth 2000)
+
+let secretSeq2 x = secretSeq x |> Seq.map (fun x -> string x |> Seq.last |> string |> int)
+
+let secretComb init = 
+    secretSeq2 init |> Seq.take 2001 |> Seq.windowed 5
+    |> Seq.map (fun x -> 
+        let d = (x[1] - x[0]) * 1000000 + (x[2] - x[1]) * 10000 + (x[3] - x[2]) * 100 + (x[4] - x[3])
+        d, (init, x[4]))
 
 let part2 lines = 
-    parseInput lines
+    parseInput lines |> Seq.collect secretComb
+    |> Seq.groupBy fst |> Seq.sortBy fst |> Seq.map (fun (k, g) -> 
+        let firstPerBuyer = g |> Seq.map snd |> Seq.groupBy fst |> Seq.map (snd >> Seq.head)
+        let v = firstPerBuyer |> Seq.map snd |> Seq.sum
+        k, v)
+    |> Seq.maxBy snd
 
 let sol = {
     Day = 22
     Part1 = solution part1 (Seq.sum >> string)
-    Part2 = solution part2 (Seq.length >> string)
+    Part2 = solution part2 (snd >> string)
 }
