5d6c2b9471c3a59fec98316530c579c4fba5b256
jindraivanek <jindra.ivanek@gmail.com>
2024-12-26 09:21:00 +0100

    day 25

--- 2024/25.fs
+++ 2024/25.fs
@@ -1,17 +1,32 @@
 module day25
+
 open Common
 
 let parseInput (lines: string[]) =
-    lines
+    let schematics = lines |> splitBy ((=) "") |> Seq.toList
+    let lockSch, keySch = schematics |> List.partition (fun s -> s[0] = "#####")
 
-let part1 lines = 
-   parseInput lines
+    let toWidths f s =
+        s
+        |> List.map (Seq.toList)
+        |> List.transpose
+        |> List.map (f >> List.tail >> List.takeWhile ((=) '#') >> List.length)
 
-let part2 lines = 
-    parseInput lines
+    let locks = lockSch |> List.map (toWidths id)
+    let keys = keySch |> List.map (toWidths List.rev)
+    locks, keys
 
-let sol = {
-    Day = 25
-    Part1 = solution part1 (Seq.length >> string)
-    Part2 = solution part2 (Seq.length >> string)
-}
+let part1 lines =
+    let locks, keys = parseInput lines
+
+    List.allPairs locks keys
+    |> List.filter (fun (l, k) ->
+        let xs = List.zip l k
+        xs |> List.forall (fun (l, k) -> l + k <= 5))
+
+let part2 lines = parseInput lines
+
+let sol =
+    { Day = 25
+      Part1 = solution part1 (Seq.length >> string)
+      Part2 = solution part2 (string) }
